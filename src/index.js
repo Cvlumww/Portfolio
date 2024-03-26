a5a281d42490e5b4b9d6145fd3da25ea53a1d2e5
Cvlumww <calumwardrop2000@gmail.com>
2024-03-26 17:38:30 +0000

    hello, just adding GA4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import ReactGA from "react-ga4";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ReactGA.initialize("G-MR784QT1CX");
+
 root.render(
   <React.StrictMode>
     <App />
